c0c51f3d2b5faa70e892f8e1bdcf3c8fb3d75879
Stephen Grice <stephengrice@live.com>
2019-05-21 22:32:13 -0400

    Generate learner app and write first test

---
+++ wordwrench/learner/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+# Create your tests here.
+class LearnerTests(TestCase):
+    def test_nothing(self):
+        self.assertTrue(False, "Write some tests, ya nut!")
