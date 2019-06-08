75cfdf300ec4a07db73e03fe5d2103b3172828d4
Stephen Grice <stephengrice@live.com>
2019-06-08 13:55:04 -0400

    Add selenium

--- wordwrench/functional_tests/tests.py
+++ wordwrench/functional_tests/tests.py
@@ -1,5 +1,16 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time
 
 class NewUserTest(LiveServerTestCase):
-    pass
+    @classmethod
+    def setUpClass(cls):
+        super(NewUserTest, cls).setUpClass()
+        options = Options()
+        options.headless = True
+        cls.selenium = webdriver.Firefox(options=options)
+
+    def test_nothing(self):
+        time.sleep(3)
+        self.assertTrue(True)
