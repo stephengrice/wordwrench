966f5396b551e47a446c84f95c1d176842662aec
Stephen Grice <stephengrice@live.com>
2019-06-08 12:03:40 -0400

    Add .travis.yml

---
+++ wordwrench/functional_tests/tests.py
@@ -0,0 +1,5 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+class NewUserTest(LiveServerTestCase):
+    pass
