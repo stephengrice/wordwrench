ed1c3c3aa7201965bb87e3029fd9b33bb210a469
Stephen Grice <stephengrice@live.com>
2019-06-20 19:57:58 -0400

    Complete initial ajax implementation for topic-learner

--- static/assistant/js/test/topic-learner.test.js
+++ static/assistant/js/test/topic-learner.test.js
@@ -17,7 +17,11 @@ describe('Topic Learner', () => {
   var learner;
   var server;
   beforeEach(function() {
-    learner = new TopicLearner($('#learner'), 'spanish', 'sample');
+    learner = new TopicLearner({
+      element: $('#learner'),
+      language: 'spanish',
+      topic: 'sample'
+    });
 
     server = sinon.fakeServer.create();
     // stubAjax = sinon.stub($, 'ajax').returns();
@@ -27,7 +31,7 @@ describe('Topic Learner', () => {
   afterEach(function() {
     server.restore();
   });
-  it('throws an error with less than three arguments', () => {
+  it('throws an error with improper params object', () => {
     assert.throws(() => {
       let learner = new TopicLearner();
     }, Error);
@@ -37,9 +41,16 @@ describe('Topic Learner', () => {
     assert.throws(() => {
       let learner = new TopicLearner($('#learner'), 'not a lang');
     }, Error);
+    assert.throws(() => {
+      let learner = new TopicLearner({});
+    }, Error);
   });
   it('instantiates successfully with element, language, and topic arguments', () => {
-    let mlearner = new TopicLearner($('#learner'), 'spanish', 'sample');
+    let mlearner = new TopicLearner({
+      element: $('#learner'),
+      language: 'spanish',
+      topic: 'sample'
+    });
   });
   it('sets elements, language, and topic fields to the ones passed in constructor', () => {
     assert.equal(learner.element.attr('id'), $('#learner').attr('id'));
@@ -81,37 +92,110 @@ describe('Topic Learner', () => {
     assert.equal(learner.topic_data, null);
     assert.equal(learner.loaded, false);
   });
-  it('sends ajax request when load() is called', () => {
-    var returnObject = {
-      done: () => {return returnObject},
-      fail: () => {}
-    };
-    sinon.replace($, 'ajax', sinon.fake.returns(returnObject));
+  describe('load()', () => {
+    it('sends ajax request when called', () => {
+      var returnObject = {
+        done: () => {return returnObject},
+        fail: () => {}
+      };
+      sinon.replace($, 'ajax', sinon.fake.returns(returnObject));
 
-    learner.load();
-    assert($.ajax.calledOnce);
-
-    sinon.restore();
-  });
-  it('throws an error if ajax not successful', () => {
-    let returnObject = {
-      done: (callback) => {
-        return returnObject;
-      },
-      fail: (callback) => {
-        callback('404 error');
-        return returnObject;
-      }
-    };
-    sinon.replace($, 'ajax', sinon.fake.returns(returnObject));
-
-    assert.throws(() => {
       learner.load();
-    });
+      assert($.ajax.calledOnce);
 
-    sinon.restore();
-  })
-});
+      sinon.restore();
+    });
+    it('throws an error if ajax not successful', () => {
+      let returnObject = {
+        done: (callback) => {
+          return returnObject;
+        },
+        fail: (callback) => {
+          callback('404 error');
+          return returnObject;
+        }
+      };
+      sinon.replace($, 'ajax', sinon.fake.returns(returnObject));
+
+      assert.throws(() => {
+        learner.load();
+      });
+
+      sinon.restore();
+    });
+    it('sends an ajax request with topic and language', () => {
+      let returnObject = {
+        done: (callback) => {
+          callback([]);
+          return returnObject;
+        },
+        fail: (callback) => {
+          return returnObject;
+        }
+      };
+      let m_fake = sinon.fake.returns(returnObject);
+      sinon.replace($, 'ajax', m_fake);
+      let callobject = {
+        url: TopicLearner.API_ENDPOINT,
+        data: {
+          language: 'spanish',
+          topic: 'sample'
+        }
+      };
+
+      learner.load();
+
+      sinon.assert.calledWith(m_fake, callobject);
+      sinon.restore();
+    });
+    it('throws an error if data received is not an array', () => {
+      let returnObject = {
+        done: (callback) => {
+          callback('not an array');
+          return returnObject;
+        },
+        fail: (callback) => {
+          return returnObject;
+        }
+      };
+      let m_fake = sinon.fake.returns(returnObject);
+      sinon.replace($, 'ajax', m_fake);
+      assert.throws(() => {
+        learner.load();
+      });
+      sinon.restore();
+    });
+    it('calls the storeData callback', () => {
+      let dataArray = ['data1','data2','data3'];
+      let returnObject = {
+        done: (callback) => {
+          callback(dataArray);
+          return returnObject;
+        },
+        fail: (callback) => {
+          return returnObject;
+        }
+      };
+      let m_fake = sinon.fake.returns(returnObject);
+      sinon.replace($, 'ajax', m_fake);
+      let m_spy = sinon.spy(learner, 'storeData');
+
+      learner.load();
+      sinon.assert.calledOnce(m_spy);
+
+      sinon.restore();
+    });
+  }); // </describe load()>
+  describe('storeData()', () => {
+    it('restores received data in topic_data', () => {
+      assert(learner.topic_data == null);
+      let dataArray = ['data1','data2','data3'];
+      learner.storeData(dataArray);
+      assert.equal(learner.topic_data, dataArray);
+    });
+  }); // </describe storeData()>
+
+}); // </describe TopicLearner>
 
 function assertHasElement(learner, element_id) {
   assert.notEqual(learner[element_id], undefined, `Element ${element_id} is undefined`);
--- static/assistant/js/topic-learner.js
+++ static/assistant/js/topic-learner.js
@@ -7,33 +7,35 @@ var INNER_HTML = ' \
   <input id="txt_answer" /> \
   <div id="lbl_grade"></div> \
   <button id="btn_next">Next</button> \
-';
+  ';
 
 var TopicLearner = (function() {
-  var TopicLearner = function(element, language, topic) {
+  var TopicLearner = function(params) {
     // Constructor
-    if (element === undefined || language === undefined || topic === undefined) {
-      throw new Error("You must provide an element, language, and topic to instantiate TopicLearner");
+    if (params === undefined || params['element'] === undefined || params['language'] === undefined || params['topic'] === undefined) {
+      throw new Error("You must provide an object with element, language, and topic to instantiate TopicLearner");
     }
-    if (element.length == 0) {
+    if (params['element'].length == 0) {
       throw new Error(`The element provided was not found in the DOM`)
     }
-    this.element = element;
+    this.element = params['element'];
     this.element.html(INNER_HTML);
     // Sub-element init
-    this.progress_bar = element.find('#progress_bar');
-    this.lbl_prompt = element.find('#lbl_prompt');
-    this.lbl_translation = element.find('#lbl_translation');
-    this.txt_answer = element.find('#txt_answer');
-    this.lbl_grade = element.find('#lbl_grade');
-    this.btn_next = element.find('#btn_next');
+    this.progress_bar = this.element.find('#progress_bar');
+    this.lbl_prompt = this.element.find('#lbl_prompt');
+    this.lbl_translation = this.element.find('#lbl_translation');
+    this.txt_answer = this.element.find('#txt_answer');
+    this.lbl_grade = this.element.find('#lbl_grade');
+    this.btn_next = this.element.find('#btn_next');
     // Fields
-    this.language = language;
-    this.topic = topic;
+    this.language = params['language'];
+    this.topic = params['topic'];
     this.topic_data = null;
     this.loaded = false;
     this.setPercentage(0.0);
   };
+
+  TopicLearner.prototype.API_ENDPOINT = '/api/topic';
 
   TopicLearner.prototype.getPercentage = function() {
     return this.percentage;
@@ -45,13 +47,25 @@ var TopicLearner = (function() {
   }
 
   TopicLearner.prototype.load = function() {
-    $.ajax(
-      '/api/topic'
-    ).done(function(data) {
-      console.log('all done');
+    let that = this; // address scope issue in done()
+    $.ajax({
+      url: TopicLearner.API_ENDPOINT,
+      data: {
+        topic: this.topic,
+        language: this.language
+      }
+    }).done(function(data) {
+      if (data === undefined || data.constructor !== Array) {
+        throw new Error('Invalid JSON response received: not an array');
+      }
+      that.storeData(data);
     }).fail(function(error) {
       throw new Error('Failed to get data: ' + error);
     });
+  };
+
+  TopicLearner.prototype.storeData = function(data) {
+    this.topic_data = data;
   };
 
   return TopicLearner;
